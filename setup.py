7e9fe36d19480a5b2a6dfe5e4cf765b88f61fd72
jamestom-cpu <66060633+jamestom-cpu@users.noreply.github.com>
2022-09-28 20:11:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from distutils.core import setup
 
+setup(
+    name = 'PWL',
+    packages = ['PWL'],
+    version = 1,  # Ideally should be same as your GitHub release tag varsion
+    description = '...',
+    author = 'tm95mon',
+    author_email = '...',
+    url = 'https://github.com/jamestom-cpu/SignalAnalysis.git',
+    download_url = 'https://github.com/jamestom-cpu/SignalAnalysis/archive/refs/tags/PWL1.tar.gz',
+    keywords = ['PWL'],
+    classifiers = [],
+)
